9d986f8bb7630e19594cfec9931eb62da68496e3
Markus Kwaśnicki <github@markus.kwasniccy.eu>
2012-08-20 13:34:24 +0200

    Version release 3.6.10.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.9.5")]
-[assembly: AssemblyFileVersion("3.6.9.5")]
+[assembly: AssemblyVersion("3.6.10.6")]
+[assembly: AssemblyFileVersion("3.6.10.6")]
